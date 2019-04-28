86f427a5f6d37b683f59092d869505638b56d47a
rema424 <rema.gapp@gmail.com>
2019-04-28 15:39:45 +0900

    不要なコードの削除

--- lib/filepath.go
+++ lib/filepath.go
@@ -4,17 +4,12 @@ import (
 	"os"
 )
 
-// GetImportPath は go get でダウンロードされたビルド前のソースコードの存在するパスを文字列で返却します。
+// GetImportPath は go get の結果ソースコードが配置されたディレクトリのパスを返却します。
 func GetImportPath() string {
 	return os.ExpandEnv("${GOPATH}/src/github.com/rema424/goat")
 }
 
-// GetTemplateDirPath は template ディレクトリのパスを文字列で返却します。
-func GetTemplateDirPath() string {
-	return GetImportPath() + "/template"
-}
-
-// GetTemplatePath はテンプレートファイルの存在する絶対パスを文字列で返却します。
+// GetTemplatePath は引数で与えられてたテンプレートファイルの存在するパスを返却します。
 func GetTemplatePath(tmplName string) string {
-	return GetTemplateDirPath() + "/" + tmplName
+	return GetImportPath() + "/template/" + tmplName
 }
